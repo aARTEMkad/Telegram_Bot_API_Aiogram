64cd1832afe057694e05f9f64e0ca730d557af97
Artem Karpuk <75688693+aARTEMkad@users.noreply.github.com>
2022-07-26 23:00:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import Config
 bot = Bot(token=Config.TOKEN)
 dp = Dispatcher(bot)
 
-temp_number = 0
-
 print("Starting...")
 
 
@@ -29,13 +27,34 @@ async def menu_1(message: types.message):
         await bot.send_message(message.from_user.id, '2', reply_markup=anv.menu_info)
     # menu_Information_Bot
     elif message.text == 'Возможности бота':
-        await bot.send_message(message.from_user.id, '2', reply_markup=anv.menu)
+        await bot.send_message(message.from_user.id, '2', reply_markup=anv.Inl_menu)
 
     elif message.text == 'Для чого был он создан':
-        await bot.send_message(message.from_user.id, '2', reply_markup=anv.menu)
+        await bot.send_message(message.from_user.id, '2', reply_markup=anv.Inl_menu)
     # menu_Other
     elif message.text == '↙ Вернуться назад':
         await bot.send_message(message.from_user.id, '2', reply_markup=anv.menu)
 
+# V1.1
+@dp.callback_query_handler(text_contains='menu_')
+async def menu(call: types.CallbackQuery):
+    if call.data and call.data.startswith("menu_"):
+        code = call.data[-1:]
+        if code.isdigit():
+            code = int(code)
+        if code == 1:
+            await call.message.edit_text('TEST_1', reply_markup=anv.Inl_menu)
+        if code == 2:
+            await call.message.edit_text('TEST_2_dalle', reply_markup=anv.Inl__2menu)
+        if code == 3:
+            await call.message.edit_text('TEST_3', reply_markup=anv.Inl_menu)
+        if code == 4:
+            await call.message.edit_text('TEST_4', reply_markup=anv.Inl__2menu)
+        if code == 5:
+            await call.message.edit_text('TEST_5', reply_markup=anv.Inl__2menu)
+        if code == 6:
+            await call.message.edit_text('TEST_6_Nazad', reply_markup=anv.Inl_menu)
+        else:
+            await bot.answer_callback_query(call.id)
 
 executor.start_polling(dp)
